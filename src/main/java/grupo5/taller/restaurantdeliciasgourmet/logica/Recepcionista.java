225f68c1d768f934d7037ef8f0b7c66eec688bb9
Am3ricoo <j0se.b3ntz@gmail.com>
2024-09-22 13:54:06 -0300

    clase recepcionista modificada

--- src/main/java/grupo5/taller/restaurantdeliciasgourmet/logica/Recepcionista.java
+++ src/main/java/grupo5/taller/restaurantdeliciasgourmet/logica/Recepcionista.java
@@ -33,14 +33,47 @@ public class Recepcionista extends Empleado {
 
     public void setReservas(ArrayList<Reserva> reservas) {
         this.reservas = reservas;
-    }    
+    }   
     
-    
-    public void asignarMesa(int idReserva, int numeroMesa){
-        
+    //permite agregar una reserva
+    public void agregarReserva(Reserva reserva) {
+        reservas.add(reserva);
     }
     
-    public void recibirCliente(){
-        
+    //permite eliminar una reserva
+    public void eliminarReserva(Reserva reserva) {
+        reservas.remove(reserva);
     }
+    
+    //permite buscar una reserva por ID
+    public Reserva buscarReservaPorId(int idReserva) {
+        for (Reserva reserva : reservas) {
+            if (reserva.getIdReserva() == idReserva) {
+             return reserva;
+            }
+        }
+        return null; // O lanzar una excepción si lo prefieres
+    }
+    
+    //confirmar una reserva
+    public void confirmarReserva(int idReserva) {
+        Reserva reserva = buscarReservaPorId(idReserva);
+        if (reserva != null) {
+            reserva.setEstadoReserva(EstadoReserva.CONFIRMADA);
+        } else {
+            // throw new ReservaNoEncontradaException("Reserva no encontrada.");
+        }
+    }
+    //muestra si ls reserva esta confirmada
+    public void recibirCliente(Cliente cliente) {
+        for (Reserva reserva : reservas) {
+            if (reserva.getCliente().equals(cliente) && reserva.getEstadoReserva() == EstadoReserva.CONFIRMADA) {
+                System.out.println("Bienvenido, " + cliente.getNombre() + ". Su reserva está confirmada.");
+                
+                return;
+            }
+        }
+        System.out.println("No se encontró ninguna reserva confirmada para " + cliente.getNombre() + ".");
+    }
+
 }
